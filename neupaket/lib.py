febc14d87799bb6863cb57b15e13252014baff10
Joachim Kolb <joachim.kolb@gmail.com>
2021-08-16 16:20:12 -0400

    Initial Push

---
+++ neupaket/lib.py
@@ -0,0 +1,9 @@
+# mlproject/lib.py
+
+def hello_world():
+    return "Hello world from mlproject"
+
+def try_me():
+    interest = input('What book would you take if you had to relocate to a desert island?')
+    return print(f"Really? So what's so special about {interest}?")
+
